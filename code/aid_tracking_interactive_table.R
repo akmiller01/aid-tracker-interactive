ad4b0a2a7db248605cbb8965bd772fabb925a00f
dean-breed <dean.breed@devinit.org>
2022-01-24 16:58:14 +0000

    Script update to remove all but comms/disbs

--- code/aid_tracking_interactive_table.R
+++ code/aid_tracking_interactive_table.R
@@ -1,12 +1,28 @@
 # Code to produce table needed for the live IATI COVID tracker page #
 
-{
-  list.of.packages <- c("data.table", "anytime", "ggplot2", "scales", "bsts", "dplyr", "plyr","Hmisc","reshape2","splitstackshape","Cairo","svglite","extrafont","jsonlite","countrycode","openxlsx","english","stringr","tidyr","rstudioapi")
-  new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-  if(length(new.packages)) install.packages(new.packages)
-  lapply(list.of.packages, require, character.only=T)
+list.of.packages <- c("curl","data.table", "anytime", "ggplot2", "scales", "bsts", "dplyr", "plyr","Hmisc","reshape2","splitstackshape","Cairo","svglite","extrafont","jsonlite","countrycode","openxlsx","english","stringr","tidyr","rstudioapi")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
   
-  wd <- dirname(getActiveDocumentContext()$path) # Setting working directory to the input. Check this is where you locally have this repository, else change it.
-  setwd(wd)
-  setwd("..")
-}
+wd <- dirname(getActiveDocumentContext()$path) # Setting working directory to the input. Check this is where you locally have this repository, else change it.
+setwd(wd)
+setwd("..")
+
+#### DDW read-in ####
+
+current_month <- month(Sys.Date()-75) # Note: This is the less than or equal to so if you want to include up to November, for example, this must say 11.
+current_year <- year(Sys.Date()-75)
+current_yyyymm <- format(Sys.Date()-75, "%Y%m") # Note: This must be preceded by a 0 if in the first nine months (i.e. 202105, not 20215)
+
+dat <- fread("https://ddw.devinit.org/api/export/1250")
+
+dat$`Transaction Type`[which(dat$`Transaction Type`=="Outgoing Commitment")] <- "Commitments"
+dat$`Transaction Type`[which(dat$`Transaction Type`=="Disbursement")] <- "Disbursements"
+
+dat$usability[which(dat$`Transaction Type`=="Commitments")] <- dat$tracker_commit[which(dat$`Transaction Type`=="Commitments")]
+dat$usability[which(dat$`Transaction Type`=="Disbursements")] <- dat$tracker_commit[which(dat$`Transaction Type`=="Disbursements")]
+
+dat[which(dat$`Transaction Type`%in%c("Commitments","Disbursements"))]
+
+write.csv(dat,"usability.csv")
